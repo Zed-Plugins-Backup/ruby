141be2f6e599586e53aaf44966b6230bdf903da8
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-11-15 17:39:01 +0100

    Show completion details for `ruby-lsp`
    The `ruby-lsp` language server sends additional
    completion details via `completion.label_details` field:
    
    ```json
    {
      "label": "eql?",
      "labelDetails": { "detail": "(other)", "description": "kernel.rbs" }
    }
    ```
    
    Starting from `zed_extension_api` v0.2.0 we can use this field
    to display additional completion details.

--- src/language_servers/ruby_lsp.rs
+++ src/language_servers/ruby_lsp.rs
@@ -76,11 +76,24 @@ impl RubyLsp {
         };
 
         let len = completion.label.len();
-        let name_span = CodeLabelSpan::literal(completion.label, Some(highlight_name.to_string()));
+        let mut spans = Vec::with_capacity(3);
+
+        spans.push(CodeLabelSpan::literal(
+            completion.label,
+            Some(highlight_name.into()),
+        ));
+
+        if let Some(detail) = completion
+            .label_details
+            .and_then(|label_details| label_details.detail)
+        {
+            spans.push(CodeLabelSpan::literal(" ", None));
+            spans.push(CodeLabelSpan::literal(detail, None));
+        }
 
         Some(CodeLabel {
             code: Default::default(),
-            spans: vec![name_span],
+            spans,
             filter_range: (0..len).into(),
         })
     }
